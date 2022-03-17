5d222744d8dbf791de17c939f1843b750e828b0f
Dean Shaked <deanshak9@gmail.com>
2022-03-17 23:25:57 +0200

    ädded kyc contract

--- contracts/MyTokenSale.sol
+++ contracts/MyTokenSale.sol
@@ -1,16 +1,25 @@
-pragma solidity ^0.6.0;
+pragma solidity ^0.6.1;
 
 import "./Crowdsale.sol";
+import "./kycContract.sol";
 
 contract MyTokenSale is Crowdsale {
+
+    KycContract kyc;
     constructor(
         uint256 rate,    // rate in TKNbits
         address payable wallet,
-        IERC20 token
+        IERC20 token,
+        KycContract _kyc
     )
         Crowdsale(rate, wallet, token)
         public
     {
+        kyc = _kyc;
+    }
 
+    function _preValidatePurchase(address beneficiary, uint256 weiAmount) internal view override {
+        super._preValidatePurchase(beneficiary,weiAmount);
+        require(kyc.kycCompleted(msg.sender),"KYC is not completed, purchase is not allowed");
     }
 }
