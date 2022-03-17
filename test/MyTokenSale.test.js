761ff709ea9f4f7939ed6d658638c0df81342696
Dean Shaked <deanshak9@gmail.com>
2022-03-17 20:15:42 +0200

    added more tests

--- test/MyTokenSale.test.js
+++ test/MyTokenSale.test.js
@@ -18,7 +18,31 @@ contract("TokenSale Test", async (accounts) => {
     ).to.eventually.be.a.bignumber.equal(new BN(0));
   });
 
-  it("all tokens should be in the TokenSale Smart Contract by default", async () => {});
+  it("all tokens should be in the TokenSale Smart Contract by default", async () => {
+    let instance = await Token.deployed();
+    let balanceOfTokenSaleSmartContract = await instance.balanceOf(
+      TokenSale.address
+    );
 
-  it("should be possible to buy tokens", async () => {});
+    let totalSupply = await instance.totlaSupply();
+
+    return expect(balanceOfTokenSaleSmartContract).to.be.a.bignumber.equal(
+      totalSupply
+    );
+  });
+
+  it("should be possible to buy tokens", async () => {
+    let tokenInstance = await Token.deployed();
+    let tokenSaleInstance = await TokenSale.deployed();
+    let balanceBefore = await tokenInstance.balanceOf(deployerAccount);
+    expect(
+      tokenSaleInstance.sendTransaction({
+        from: deployerAccount,
+        value: web3.utils.toWei("1", "wei"),
+      })
+    );
+    return expect(
+      tokenInstance.balanceOf(deployerAccount)
+    ).to.eventually.be.a.bignumber.equal(balanceBefore.add(new BN(1)));
+  });
 });
